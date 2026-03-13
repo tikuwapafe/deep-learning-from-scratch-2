aa4875fb6788150add0b1beb06687a4d2eb83e52
tikuwapafe <sanonanari@gmail.com>
2026-03-13 13:24:09 +0900

    ch04

--- ch04/train.py
+++ ch04/train.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-import sys
-sys.path.append('..')
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from common import config
 # GPUで実行する場合は、下記のコメントアウトを消去（要cupy）
 # ===============================================
